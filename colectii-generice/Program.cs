91a6e68a3266b1e3b5e03f0d938a0bbe74efa3dc
nedelcualexioan <nedelcuioan69@gmail.com>
2022-06-05 20:45:26 +0300

    added set

--- colectii-generice/Program.cs
+++ colectii-generice/Program.cs
@@ -7,23 +7,13 @@ namespace colectii_generice
     {
         static void Main(string[] args)
         {
-            Masina m = new Masina("BMW", "X5", "Albastru", 2009);
-            Masina m2 = new Masina("Tesla", "ModelX", "Alb", 2021);
+            Set<int> set = new Set<int>();
 
-            Lista<Masina> list = new Lista<Masina>();
-
-            list.addStart(m);
-            list.addStart(m2);
-
-            Masina m3 = new Masina("Dacia", "Logan", "Alb", 2005);
-
-            list.add(m3);
-
-            list.set(new Masina("Dacia", "Sandero", "Alb", 2005), 2);
-
-            list.sortCr();
-
-            list.afisare();
+            set.add(1);
+            set.add(3);
+            set.add(10);
+            set.add(5);
+            set.add(4);
         }
     }
 }
---
+++ colectii-generice/colectii/Set.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using colectii_generice.colectii;
+
+namespace colectii_generice.colectii
+{
+    class Set<T> where T :IComparable<T>
+    {
+        private Node<T> head;
+
+        public bool add(T data)
+        {
+            if (head == null)
+            {
+                head = new Node<T>
+                {
+                    Data = data,
+                    Next = null
+                };
+
+                return true;
+            }
+
+            Node<T> enumerator = head;
+
+            while (data.CompareTo(enumerator.Data) > 0)
+            {
+                enumerator = enumerator.Next;
+            }
+
+            if (data.CompareTo(enumerator.Data) == 0)
+                return false;
+
+            Node<T> node = new Node<T>(data, enumerator.Next);
+
+            enumerator.Next = node;
+
+            return true;
+        }
+    }
+}
